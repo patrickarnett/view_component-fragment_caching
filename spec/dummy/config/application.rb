8c069b20db7ec5c76e7a38743e7efeeb3ad60c14
Patrick Arnett <parnett@g2.com>
2022-06-16 16:08:28 -0400

    Remove unnecessary dependencies

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'action_controller/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
